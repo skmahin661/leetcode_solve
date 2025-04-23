e189dff0abc8320f54e8aadae98f486559de68a3
Munshaibur Rahman Mahin <159491891+skmahin661@users.noreply.github.com>
2025-04-24 00:59:18 +0600

    Time: 1 ms (25.05%), Space: 9.2 MB (24.97%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,35 +1,37 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        string x = "";
-        int flag = 0, dlag = 0, mlag = 0;
-
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] == ' ' && !flag) {
+        string x="";
+        int flag=0,dlag=0,mlag=0;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]==' ' && !flag && !dlag && !mlag) 
+            {
                 continue;
             }
-            if (s[i] == '-' && !flag && !dlag && !mlag) {
-                dlag = 1;
-                flag++;
+            if(s[i]=='-' && !flag && !dlag && !mlag)
+            {
+                dlag=1;
                 continue;
             }
-            if (s[i] == '+' && !flag && !dlag && !mlag) {
-                mlag = 1;
-                flag++;
+             if(s[i]=='+' && !flag && !mlag && !dlag)
+            {
+                mlag=1;
                 continue;
             }
-            if (!isdigit(s[i])) {
+            if(!isdigit(s[i]))
+            {
                 break;
             }
-            x += s[i];
+            x+=s[i];
             flag++;
         }
-
-        long long sk = 0;
-        for (int i = 0; i < x.size(); i++) {
-            int d = x[i] - '0';
-            sk = sk * 10 + d;
-            if (!dlag && sk > INT_MAX) return INT_MAX;
+        long long sk=0;
+        for(int i=0;i<x.size();i++)
+        {
+            int d = x[i]-'0';
+            sk = sk*10+d;
+         if (!dlag && sk > INT_MAX) return INT_MAX;
             if (dlag && -sk < INT_MIN) return INT_MIN;
         }
 
